42737e2f23f789425cc27ed7959cc689cd607392
cryyy002 <81438733+cryyy002@users.noreply.github.com>
2022-01-03 16:54:25 +0300

    Add files via upload

---
+++ Terraria/GameContent/Drawing/ParticleOrchestraType.cs
@@ -0,0 +1,15 @@
+namespace Terraria.GameContent.Drawing
+{
+	public enum ParticleOrchestraType : byte
+	{
+		Keybrand,
+		FlameWaders,
+		StellarTune,
+		WallOfFleshGoatMountFlames,
+		BlackLightningHit,
+		RainbowRodHit,
+		BlackLightningSmall,
+		StardustPunch,
+		PrincessWeapon
+	}
+}
